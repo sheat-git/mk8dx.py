3fdb13b20dd47f7143bc6ca20c890405e7c2c38d
sheat <79030436+sheat-git@users.noreply.github.com>
2022-04-25 04:20:12 +0900

    Update setup.py

--- セットアップ.py
+++ セットアップ.py
@@ -8,6 +8,6 @@ def _requires_from_file(filename):
 setup(
     name='mk8dx',
     packages=['mk8dx'],
-    version='1.0',
+    version='1.0.1',
     install_requires=_requires_from_file('requirements.txt')
 )
